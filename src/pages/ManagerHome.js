e7f59e0aa9d1fa73e7ab5415f8497f3c6982dd84
Vishal <annamaneni.v@northeastern.edu>
2022-01-31 09:57:26 -0800

    Implemented Manager Overview and Manager Properties

---
+++ src/pages/ManagerHome.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import Footer from '../components/Footer';
+import AppContext from '../AppContext';
+import ManagerOverview from "./ManagerOverview";
+
+function ManagerHome() {
+    const navigate = useNavigate();
+    const {userData} = React.useContext(AppContext);
+    const [footerTab, setFooterTab] = React.useState('DASHBOARD');
+    const [showFooter, setShowFooter] = React.useState(true);
+    React.useEffect(() => {
+        if (userData.access_token === null) {
+            navigate('/');
+        }
+    }, []);
+    return (
+        <div className='d-flex flex-column h-100'>
+            <div className='flex-grow-1'>
+                {footerTab === 'DASHBOARD' ? (
+                    <ManagerOverview setShowFooter={setShowFooter}/>
+                ) : ''}
+            </div>
+            {showFooter ? (
+                <Footer tab={footerTab} setTab={setFooterTab}/>
+            ) : ''}
+        </div>
+    );
+}
+
+export default ManagerHome;
