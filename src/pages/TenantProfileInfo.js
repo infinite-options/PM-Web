bdc8b79f1bb60445eced0ce88c22697ae0c33106
zlind1 <62447522+zlind1@users.noreply.github.com>
2022-03-01 23:04:47 -0800

    named documents for contracts, rentals, tenant profile

--- src/pages/TenantProfileInfo.js
+++ src/pages/TenantProfileInfo.js
@@ -9,6 +9,8 @@ import AddressForm from "../components/AddressForm";
 import { get, post } from "../utils/api";
 import ArrowUp from "../icons/ArrowUp.svg";
 import ArrowDown from "../icons/ArrowDown.svg";
+import EditIcon from '../icons/EditIcon.svg';
+import DeleteIcon from '../icons/DeleteIcon.svg';
 import {
   squareForm,
   pillButton,
@@ -16,6 +18,8 @@ import {
   underline,
   red,
   hidden,
+  mediumBold,
+  smallPillButton,
 } from "../utils/styles";
 
 function TenantProfileInfo(props) {
@@ -66,7 +70,7 @@ function TenantProfileInfo(props) {
     rent: "",
   });
 
- 
+
   const previousAddressState = React.useState({
     street: "",
     unit: "",
@@ -79,7 +83,53 @@ function TenantProfileInfo(props) {
     lease_end: "",
     rent: "",
   });
- 
+
+  const [newFile, setNewFile] = React.useState(null);
+  const [editingDoc, setEditingDoc] = React.useState(null);
+  const [files, setFiles] = React.useState([]);
+  const addFile = (e) => {
+    const file = e.target.files[0];
+    const newFile = {
+      name: file.name,
+      description: '',
+      file: file
+    }
+    setNewFile(newFile);
+  }
+  const updateNewFile = (field, value) => {
+    const newFileCopy = {...newFile};
+    newFileCopy[field] = value;
+    setNewFile(newFileCopy);
+  }
+  const cancelEdit = () => {
+    setNewFile(null);
+    if (editingDoc !== null) {
+      const newFiles = [...files];
+      newFiles.push(editingDoc);
+      setFiles(newFiles);
+    }
+    setEditingDoc(null);
+  }
+  const editDocument = (i) => {
+    const newFiles = [...files];
+    const file = newFiles.splice(i, 1)[0];
+    setFiles(newFiles);
+    setEditingDoc(file);
+    setNewFile({...file});
+  }
+  const saveNewFile = (e) => {
+    // copied from addFile, change e.target.files to state.newFile
+    const newFiles = [...files];
+    newFiles.push(newFile);
+    setFiles(newFiles);
+    setNewFile(null);
+  }
+  const deleteDocument = (i) => {
+    const newFiles = [...files];
+    newFiles.splice(i, 1);
+    setFiles(newFiles);
+  }
+
   React.useEffect(() => {
       if (access_token === null) {
         navigate("/");
@@ -111,7 +161,7 @@ function TenantProfileInfo(props) {
       // currentAddressState[0].lease_start === "" ||
       // currentAddressState[0].lease_end === "" ||
       // currentAddressState[0].rent === "";
-    
+
     previousAddressState[0].state = selectedPrevState;
 
     const previousAddressInvalid =
@@ -150,7 +200,13 @@ function TenantProfileInfo(props) {
       current_address: currentAddressState[0],
       previous_address: usePreviousAddress ? previousAddressState[0] : null
     }
-    await post('/tenantProfileInfo', tenantProfile, access_token);
+    for (let i = 0; i < files.length; i++) {
+      let key = `doc_${i}`;
+      tenantProfile[key] = files[i].file;
+      delete files[i].file;
+    }
+    tenantProfile.documents = JSON.stringify(files);
+    await post('/tenantProfileInfo', tenantProfile, access_token, files);
     updateAutofillState(tenantProfile);
     props.onConfirm();
   };
@@ -311,11 +367,11 @@ function TenantProfileInfo(props) {
                     />
                 </Form.Group>
             </Col>
-            
+
             <Col
                 className="px-0"
                 onClick={(e) => {
-                  
+
                   handleClick(e);
                 }}
               >
@@ -330,7 +386,7 @@ function TenantProfileInfo(props) {
                   <DropdownButton variant="light" id="dropdown-basic-button" title={frequency} >
                      {allFrequency.map((freq, i) => (
                      <Dropdown.Item onClick={() => setFrequency(freq)} href="#/action-1" > {freq} </Dropdown.Item>
-                     ))}  
+                     ))}
                  </DropdownButton>
                 </Form.Group>
             </Col>
@@ -409,7 +465,7 @@ function TenantProfileInfo(props) {
               {/* <DropdownButton variant="light" id="dropdown-basic-button" title={selectedDlState} style={{maxHeight:"90%",overflowY:"auto"}}>
                 {usDlStates.map((state, i) => (
                   <Dropdown.Item onClick={() => setSelectedDlState(state.abbreviation)} href="#/action-1" > {state.abbreviation} </Dropdown.Item>
-                  ))}  
+                  ))}
               </DropdownButton> */}
               {/* <Form.Control style={squareForm} placeholder='CA' value={dlState}
                 onChange={(e) => setDLState(e.target.value)}/> */}
@@ -455,6 +511,71 @@ function TenantProfileInfo(props) {
           ) : (
             ""
           )}
+
+        <div className='mb-4'>
+          <h5 style={mediumBold}>Tenant Documents</h5>
+          {files.map((file, i) => (
+            <div key={i}>
+              <div className='d-flex justify-content-between align-items-end'>
+                <div>
+                  <h6 style={mediumBold}>
+                    {file.name}
+                  </h6>
+                  <p style={small} className='m-0'>
+                    {file.description}
+                  </p>
+                </div>
+                <div>
+                  <img src={EditIcon} alt='Edit' className='px-1 mx-2'
+                    onClick={() => editDocument(i)}/>
+                  <img src={DeleteIcon} alt='Delete' className='px-1 mx-2'
+                    onClick={() => deleteDocument(i)}/>
+                  <a href={file.link} target='_blank'>
+                    <img src={File}/>
+                  </a>
+                </div>
+              </div>
+              <hr style={{opacity: 1}}/>
+            </div>
+          ))}
+          {newFile !== null ? (
+            <div>
+              <Form.Group>
+                <Form.Label as='h6' className='mb-0 ms-2'>
+                  Document Name
+                </Form.Label>
+                <Form.Control style={squareForm} value={newFile.name} placeholder='Name'
+                  onChange={(e) => updateNewFile('name', e.target.value)}/>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label as='h6' className='mb-0 ms-2'>
+                  Description
+                </Form.Label>
+                <Form.Control style={squareForm} value={newFile.description} placeholder='Description'
+                  onChange={(e) => updateNewFile('description', e.target.value)}/>
+              </Form.Group>
+              <div className='text-center my-3'>
+                <Button variant='outline-primary' style={smallPillButton} as='p'
+                  onClick={cancelEdit} className='mx-2'>
+                  Cancel
+                </Button>
+                <Button variant='outline-primary' style={smallPillButton} as='p'
+                  onClick={saveNewFile} className='mx-2'>
+                  Save Document
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <input id='file' type='file' accept='image/*,.pdf' onChange={addFile} className='d-none'/>
+              <label htmlFor='file'>
+                <Button variant='outline-primary' style={smallPillButton} as='p'>
+                  Add Document
+                </Button>
+              </label>
+            </div>
+          )}
+        </div>
 
         <div className="text-center" style={errorMessage === "" ? hidden : {}}>
           <p style={{ ...red, ...small }}>{errorMessage || "error"}</p>
