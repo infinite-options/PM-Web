6b9fa21e9bfefd776cd781b6d09fdc826f32c75d
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-03-03 16:21:17 -0800

    added social login and sign up

---
+++ src/pages/SocialLogin.js
@@ -0,0 +1,363 @@
+import React, { useContext, useState, useEffect } from "react";
+
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import GoogleLogin from "react-google-login";
+import axios from "axios";
+import { Grid, Button } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import { Col, Container, Form, Modal, Row } from "react-bootstrap";
+import Apple from "../icons/AppleLogin.svg";
+import Google from "../icons/GoogleLogin.svg";
+import AppContext from "../AppContext";
+import Header from "../components/Header";
+import SelectRole from "../components/SelectRole";
+import { get, post } from "../utils/api";
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const CLIENT_SECRET = process.env.REACT_APP_GOOGLE_CLIENT_SECRET;
+const GOOGLE_LOGIN = process.env.REACT_APP_GOOGLE_LOGIN;
+const useStyles = makeStyles({
+  textFieldBackgorund: {
+    backgroundColor: "#F3F3F8",
+    border: "2px solid #636366",
+    borderRadius: "3px",
+  },
+});
+
+function SocialLogin(props) {
+  const classes = useStyles();
+  // const history = useHistory();
+  const navigate = useNavigate();
+  const [loginStage, setLoginStage] = useState("LOGIN");
+  const [socialSignUpModalShow, setSocialSignUpModalShow] = useState(false);
+  const [newEmail, setNewEmail] = useState("");
+  const [newFName, setNewFName] = useState("");
+  const [newLName, setNewLName] = useState("");
+  const [socialId, setSocialId] = useState("");
+  const [refreshToken, setrefreshToken] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [accessExpiresIn, setaccessExpiresIn] = useState("");
+  const { userData, updateUserData } = useContext(AppContext);
+
+  useEffect(() => {
+    if (userData.access_token !== null) {
+      setLoginStage("ROLE");
+    }
+  }, []);
+  const socialGoogle = async (e) => {
+    const user = {
+      email: e,
+      password: GOOGLE_LOGIN,
+    };
+    const response = await post("/login", user);
+    if (response.code !== 200) {
+      setSocialSignUpModalShow(!socialSignUpModalShow);
+      return;
+      // add validation
+    }
+    console.log("login", response.result);
+    updateUserData(response.result);
+    // save to app state / context
+    setLoginStage("ROLE");
+  };
+  const responseGoogle = (response) => {
+    console.log("response", response);
+
+    let auth_code = response.code;
+    let authorization_url = "https://accounts.google.com/o/oauth2/token";
+
+    console.log("auth_code", auth_code);
+    var details = {
+      code: auth_code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: "http://localhost:3000",
+      //redirectUri: "https://skedul.online",
+      grant_type: "authorization_code",
+    };
+
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    fetch(authorization_url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: formBody,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log(responseData);
+        return responseData;
+      })
+      .then((data) => {
+        console.log(data);
+        let at = data["access_token"];
+        let rt = data["refresh_token"];
+        let ax = data["expires_in"].toString();
+        setAccessToken(at);
+        setrefreshToken(rt);
+        setaccessExpiresIn(ax);
+        console.log("res", at, rt);
+
+        axios
+          .get(
+            "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" +
+              at
+          )
+          .then((response) => {
+            console.log(response.data);
+
+            let data = response.data;
+            //setUserInfo(data);
+            let e = data["email"];
+            let fn = data["given_name"];
+            let ln = data["family_name"];
+            let si = data["id"];
+
+            setNewEmail(e);
+            setNewFName(fn);
+            setNewLName(ln);
+            setSocialId(si);
+
+            socialGoogle(e);
+          })
+          .catch((error) => {
+            console.log("its in landing page");
+            console.log(error);
+          });
+
+        // setSocialSignUpModalShow(!socialSignUpModalShow);
+
+        return (
+          accessToken,
+          refreshToken,
+          accessExpiresIn,
+          newEmail,
+          newFName,
+          newLName,
+          socialId
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log(newEmail);
+
+  const hideSignUp = () => {
+    //setSignUpModalShow(false);
+    setSocialSignUpModalShow(false);
+    navigate("/");
+    setNewFName("");
+    setNewLName("");
+  };
+
+  const socialSignUpModal = () => {
+    const modalStyle = {
+      position: "absolute",
+      top: "30%",
+      left: "2%",
+      width: "400px",
+    };
+    const headerStyle = {
+      border: "none",
+      textAlign: "center",
+      display: "flex",
+      alignItems: "center",
+      fontSize: "20px",
+      fontWeight: "bold",
+      color: "#2C2C2E",
+      textTransform: "uppercase",
+      backgroundColor: " #F3F3F8",
+    };
+    const footerStyle = {
+      border: "none",
+      backgroundColor: " #F3F3F8",
+    };
+    const bodyStyle = {
+      backgroundColor: " #F3F3F8",
+    };
+    const colHeader = {
+      margin: "5px",
+    };
+    return (
+      <Modal
+        show={socialSignUpModalShow}
+        onHide={hideSignUp}
+        style={modalStyle}
+      >
+        <Form as={Container}>
+          <Modal.Header style={headerStyle} closeButton>
+            <Modal.Title>User Does Not Exist</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body style={bodyStyle}>
+            <div>The user {newEmail} does not exist! Please Sign Up!</div>
+          </Modal.Body>
+
+          <Modal.Footer style={footerStyle}>
+            <Row
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: "1rem",
+              }}
+            >
+              <Col
+                xs={6}
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  type="submit"
+                  onClick={hideSignUp}
+                  style={{
+                    marginTop: "1rem",
+                    width: "93px",
+                    height: "40px",
+
+                    font: "normal normal normal 18px/21px SF Pro Display",
+                    letterSpacing: "0px",
+                    color: "#F3F3F8",
+                    textTransform: "none",
+                    background: "#2C2C2E 0% 0% no-repeat padding-box",
+                    borderRadius: "3px",
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Col>
+              <Col
+                xs={6}
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Button
+                  type="submit"
+                  onClick={() => navigate("/signup")}
+                  style={{
+                    marginTop: "1rem",
+                    width: "93px",
+                    height: "40px",
+
+                    font: "normal normal normal 18px/21px SF Pro Display",
+                    letterSpacing: "0px",
+                    color: "#2C2C2E",
+                    textTransform: "none",
+                    border: " 2px solid #2C2C2E",
+                    borderRadius: " 3px",
+                  }}
+                >
+                  Sign Up
+                </Button>
+              </Col>
+            </Row>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    );
+  };
+  return (
+    <Grid
+      container
+      spacing={3}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+    >
+      {loginStage === "LOGIN" ? (
+        <div>
+          <div>
+            <Button>
+              <GoogleLogin
+                clientId={CLIENT_ID}
+                accessType="offline"
+                prompt="consent"
+                responseType="code"
+                buttonText="Log In"
+                ux_mode="redirect"
+                redirectUri="http://localhost:3000"
+                //redirectUri="https://skedul.online"
+                scope="https://www.googleapis.com/auth/calendar"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                isSignedIn={false}
+                disable={true}
+                cookiePolicy={"single_host_origin"}
+                render={(renderProps) => (
+                  <img
+                    src={Apple}
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    alt={""}
+                    className="m-1"
+                  ></img>
+                )}
+              />
+            </Button>
+          </div>
+          <div>
+            <Button>
+              <GoogleLogin
+                clientId={CLIENT_ID}
+                accessType="offline"
+                prompt="consent"
+                responseType="code"
+                buttonText="Log In"
+                ux_mode="redirect"
+                redirectUri="http://localhost:3000"
+                //redirectUri="https://skedul.online"
+                scope="https://www.googleapis.com/auth/calendar"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                isSignedIn={false}
+                disable={true}
+                cookiePolicy={"single_host_origin"}
+                render={(renderProps) => (
+                  <img
+                    src={Google}
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    alt={""}
+                    className="m-1"
+                  ></img>
+                )}
+              />
+            </Button>
+          </div>
+        </div>
+      ) : loginStage === "ROLE" ? (
+        <div className="d-flex flex-column h-100 pb-5">
+          <Header title="Login" />
+          <SelectRole />
+        </div>
+      ) : (
+        ""
+      )}
+
+      {socialSignUpModal()}
+    </Grid>
+  );
+}
+
+export default SocialLogin;
