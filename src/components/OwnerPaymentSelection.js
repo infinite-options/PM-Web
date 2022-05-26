d35ff8609f5ec4debd2db5c6512c3bcc2083abea
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-05-26 03:15:25 -0700

    updated ownerprofile to match managerProfile

--- src/components/OwnerPaymentSelection.js
+++ src/components/OwnerPaymentSelection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import Checkbox from "./Checkbox";
 import { squareForm, hidden, gray, headings } from "../utils/styles";
@@ -7,18 +7,18 @@ function OwnerPaymentSelection(props) {
   const { editProfile, paymentState, setPaymentState } = props;
   const { paypal, applePay, zelle, venmo, accountNumber, routingNumber } =
     paymentState;
-  const [showPayPal, setShowPayPal] = React.useState(false);
-  const [showApplePay, setShowApplePay] = React.useState(false);
-  const [showZelle, setShowZelle] = React.useState(false);
-  const [showVenmo, setShowVenmo] = React.useState(false);
-  const [showChecking, setShowChecking] = React.useState(false);
+  const [showPayPal, setShowPayPal] = useState(false);
+  const [showApplePay, setShowApplePay] = useState(false);
+  const [showZelle, setShowZelle] = useState(false);
+  const [showVenmo, setShowVenmo] = useState(false);
+  const [showChecking, setShowChecking] = useState(false);
   const onChange = (event, field) => {
     const newPaymentState = { ...paymentState };
     newPaymentState[field] = event.target.value;
     setPaymentState(newPaymentState);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     setShowPayPal(paymentState.paypal !== "");
     setShowApplePay(paymentState.applePay !== "");
     setShowZelle(paymentState.zelle !== "");
