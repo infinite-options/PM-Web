ccc221c3759f4df0157c18e55118ec7fd30ae9e7
Vishal <annamaneni.v@northeastern.edu>
2022-06-06 07:24:07 -0700

    Added Due date and split even in manager utilities

--- src/pages/ManagerUtilities.js
+++ src/pages/ManagerUtilities.js
@@ -13,48 +13,80 @@ import {
     headings,
 } from '../utils/styles';
 import ArrowDown from "../icons/ArrowDown.svg";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import Checkbox from "../components/Checkbox";
+import Header from "../components/Header";
 
 function ManagerUtilities(props) {
     const location = useLocation();
+    const navigate = useNavigate();
     const properties = location.state.properties
 
     const [utilityState, setUtilityState] = React.useState([]);
     const [newUtility, setNewUtility] = React.useState(null);
     const [editingUtility, setEditingUtility] = React.useState(null);
     const [propertyState, setPropertyState] = React.useState(properties);
+    const [dueDate, setDueDate] = React.useState("");
 
     const emptyUtility = {
         service_name: '',
         charge: '',
         properties: [],
-        split_type: 'even'
+        split_type: 'even',
+        due_date: ""
     }
     const [errorMessage, setErrorMessage] = React.useState('');
 
 
     React.useEffect(() => {
-        console.log(properties)
+        // console.log(properties)
         properties.forEach((p) => p.checked = false)
     }, [properties]);
 
+    const splitFees = (newUtility) => {
+        let charge = parseFloat(newUtility.charge)
+
+        if (newUtility.split_type === 'even') {
+            let count = newUtility.properties.length
+            let charge_per = charge / count
+            newUtility.properties.forEach((p) => p.charge = charge_per)
+        }
+
+        if (newUtility.split_type === 'tenant') {
+
+        }
+
+        if (newUtility.split_type === 'area') {
+
+        }
+    }
+
     const addUtility = () => {
-        if (newUtility.service_name === '' || newUtility.charge === '') {
+        if (newUtility.service_name === '' || newUtility.charge === '' || newUtility.due_date === '') {
             setErrorMessage('Please fill out all fields');
             return;
         }
-
         if (propertyState.filter((p) => p.checked).length < 1) {
             setErrorMessage('Select at least one property');
             return;
         }
-        newUtility.properties = propertyState
+
+        let due_date = new Date(newUtility.due_date);
+        let current_date = new Date();
+        if (current_date >= due_date) {
+            setErrorMessage("Select a Due by Date later than current date");
+            return;
+        }
+        setErrorMessage("");
+
+        newUtility.properties = propertyState.filter((p) => p.checked)
+        splitFees(newUtility)
+        console.log('after split')
+        console.log(newUtility)
         const newUtilityState = [...utilityState];
         newUtilityState.push({...newUtility});
         setUtilityState(newUtilityState);
         setNewUtility(null);
-        setErrorMessage('');
     }
     const cancelEdit = () => {
         setNewUtility(null);
@@ -94,113 +126,137 @@ function ManagerUtilities(props) {
         ) : ''
     );
     return (
-        <Container className='px-2'>
-            <Row className='my-4'>
-                <div style={headings}>Utilities</div>
-            </Row>
-            {utilityState.length > 0 && utilityState.map((utility, i) => (
-                <div key={i}>
-                    <div className='d-flex'>
-                        <div className='flex-grow-1'>
-                            <h6 className='mb-1'>
-                                ${utility.charge} {utility.service_name} Fee &nbsp;
-                                {utility.split_type === 'even' ? 'Split Evenly' : ''}
-                                {utility.split_type === 'tenant' ? 'Split based on Tenant Count' : ''}
-                                {utility.split_type === 'area' ? 'Split based on Square Footage' : ''}
-                            </h6>
-                        </div>
-                        <div>
-                            <img src={EditIcon} alt='Edit' className='px-1 mx-2'
-                                 onClick={() => editUtility(i)}/>
-                            <img src={DeleteIcon} alt='Delete' className='px-1 mx-2'
-                                 onClick={() => deleteUtility(i)}/>
-                        </div>
-                    </div>
-                    {utility.properties.map((property, j) =>
-                        <p key={j} style={gray} className='mb-1'>
-                            {property.address} {property.unit}
-                            ,&nbsp;{property.city},&nbsp;{property.state}&nbsp; {property.zip}
-                        </p>
-                    )}
-                    <hr className='mt-1'/>
-                </div>
-            ))}
+        <div className="h-100">
 
-            {newUtility !== null ? (
-                <div>
-                    <Row className='mb-2'>
-                        <Col>
-                            <Form.Group className='mx-2'>
-                                <Form.Label as='h6' className='mb-0 ms-2'>
-                                    Utility Name {newUtility.service_name === '' ? required : ''}
-                                </Form.Label>
-                                <Form.Control style={squareForm} placeholder='Electricity' value={newUtility.service_name}
-                                              onChange={(e) => changeNewUtility(e, 'service_name')}/>
-                            </Form.Group>
-                        </Col>
+            <Header title="Utilities" leftText="< Back" leftFn={() => {navigate(-1)}}/>
 
-                        <Col>
-                            <Form.Group className='mx-2'>
-                                <Form.Label as='h6' className='mb-0 ms-2'>
-                                    Charge {newUtility.charge === '' ? required : ''}
-                                </Form.Label>
-                                <Form.Control style={squareForm} type="number" placeholder='20' value={newUtility.charge}
-                                              onChange={(e) => changeNewUtility(e, 'charge')}/>
-                            </Form.Group>
-                        </Col>
-                    </Row>
+            <Container className='px-2'>
+                <Row className='my-4'>
+                    <div style={headings}>Utilities</div>
+                </Row>
 
-                    <Form.Group className='mx-2 my-3'>
-                        <Form.Label as='h6' className='mb-0 ms-2'>
-                            Fee Distribution
-                        </Form.Label>
-                        <Form.Select
-                            style={{...squareForm, backgroundImage: `url(${ArrowDown})`,}}
-                            value={newUtility.split_type}
-                            onChange={(e) => changeNewUtility(e, 'split_type')}>
-                            <option value='even'>Uniform</option>
-                            <option value='tenant'>By Tenant Count</option>
-                            <option value='area'>By Square Footage</option>
-                        </Form.Select>
-                    </Form.Group>
-
-                    <Container className='my-3'>
-                        <h6>Properties</h6>
-                        {properties.map((property, i) => (
-                            <div key={i} className='d-flex ps-2 align-items-center my-2'>
-                                <Checkbox type='BOX' checked={propertyState[i].checked}
-                                          onClick={ () => toggleProperty(i)}/>
-                                <p className='ms-1 mb-1'>{property.address} {property.unit}
-                                    ,&nbsp;{property.city},&nbsp;{property.state}&nbsp; {property.zip}</p>
+                {utilityState.length > 0 && utilityState.map((utility, i) => (
+                    <div key={i}>
+                        <div className='d-flex'>
+                            <div className='flex-grow-1'>
+                                <h6 className='mb-1'>
+                                    ${utility.charge} {utility.service_name} Fee &nbsp;
+                                    {utility.split_type === 'even' ? 'Split Evenly' : ''}
+                                    {utility.split_type === 'tenant' ? 'Split based on Tenant Count' : ''}
+                                    {utility.split_type === 'area' ? 'Split based on Square Footage' : ''}
+                                </h6>
                             </div>
-                        ))}
-                    </Container>
-
-                    <div className='text-center my-2' style={errorMessage === '' ? hidden : {}}>
-                        <p style={{...red, ...small}}>{errorMessage || 'error'}</p>
+                            <div>
+                                {/*<img src={EditIcon} alt='Edit' className='px-1 mx-2'*/}
+                                {/*     onClick={() => editUtility(i)}/>*/}
+                                {/*<img src={DeleteIcon} alt='Delete' className='px-1 mx-2'*/}
+                                {/*     onClick={() => deleteUtility(i)}/>*/}
+                            </div>
+                        </div>
+                        {utility.properties.map((property, j) =>
+                            <p key={j} style={gray} className='mb-1'>
+                                {property.address} {property.unit}
+                                ,&nbsp;{property.city},&nbsp;{property.state}&nbsp; {property.zip}
+                            </p>
+                        )}
+                        <hr className='mt-1'/>
                     </div>
-                </div>
-            ) : ''}
+                ))}
 
-            {newUtility === null ? (
-                <Button variant='outline-primary' style={smallPillButton}
-                        onClick={() => setNewUtility({...emptyUtility})}>
-                    Add Utility
-                </Button>
-            ) : (
-                <div className='d-flex justify-content-center mb-4'>
-                    <Button variant='outline-primary' style={pillButton} onClick={cancelEdit}
-                            className='mx-2'>
-                        Cancel
-                    </Button>
-                    <Button variant='outline-primary' style={pillButton} onClick={addUtility}
-                            className='mx-2'>
+                {newUtility !== null ? (
+                    <div>
+                        <Row className='mb-2'>
+                            <Col>
+                                <Form.Group className='mx-2'>
+                                    <Form.Label as='h6' className='mb-0 ms-2'>
+                                        Utility Name {newUtility.service_name === '' ? required : ''}
+                                    </Form.Label>
+                                    <Form.Control style={squareForm} placeholder='Electricity' value={newUtility.service_name}
+                                                  onChange={(e) => changeNewUtility(e, 'service_name')}/>
+                                </Form.Group>
+                            </Col>
+
+                            <Col>
+                                <Form.Group className='mx-2'>
+                                    <Form.Label as='h6' className='mb-0 ms-2'>
+                                        Charge {newUtility.charge === '' ? required : ''}
+                                    </Form.Label>
+                                    <Form.Control style={squareForm} type="number" placeholder='20' value={newUtility.charge}
+                                                  onChange={(e) => changeNewUtility(e, 'charge')}/>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <Form.Group className='mx-2 my-3'>
+                                    <Form.Label as='h6' className='mb-0 ms-2'>
+                                        Fee Distribution
+                                    </Form.Label>
+                                    <Form.Select
+                                        style={{...squareForm, backgroundImage: `url(${ArrowDown})`,}}
+                                        value={newUtility.split_type}
+                                        onChange={(e) => changeNewUtility(e, 'split_type')}>
+                                        <option value='even'>Uniform</option>
+                                        <option value='tenant'>By Tenant Count</option>
+                                        <option value='area'>By Square Footage</option>
+                                    </Form.Select>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className="mx-2 my-3">
+                                    <Form.Label as="h6" className="mb-0 ms-2">
+                                        Due by Date {dueDate === "" ? required : ""}
+                                    </Form.Label>
+                                    <Form.Control
+                                        style={squareForm}
+                                        type="date"
+                                        value={dueDate}
+                                        onChange={(e) => changeNewUtility(e, 'due_date')}
+                                    />
+                                </Form.Group>
+                            </Col>
+                        </Row>
+
+
+                        <Container className='my-3'>
+                            <h6>Properties</h6>
+                            {properties.map((property, i) => (
+                                <div key={i} className='d-flex ps-2 align-items-center my-2'>
+                                    <Checkbox type='BOX' checked={propertyState[i].checked}
+                                              onClick={ () => toggleProperty(i)}/>
+                                    <p className='ms-1 mb-1'>{property.address} {property.unit}
+                                        ,&nbsp;{property.city},&nbsp;{property.state}&nbsp; {property.zip}</p>
+                                </div>
+                            ))}
+                        </Container>
+
+                        <div className='text-center my-2' style={errorMessage === '' ? hidden : {}}>
+                            <p style={{...red, ...small}}>{errorMessage || 'error'}</p>
+                        </div>
+                    </div>
+                ) : ''}
+
+                {newUtility === null ? (
+                    <Button variant='outline-primary' style={smallPillButton}
+                            onClick={() => setNewUtility({...emptyUtility})}>
                         Add Utility
                     </Button>
-                </div>
-            )}
+                ) : (
+                    <div className='d-flex justify-content-center mb-4'>
+                        <Button variant='outline-primary' style={pillButton} onClick={cancelEdit}
+                                className='mx-2'>
+                            Cancel
+                        </Button>
+                        <Button variant='outline-primary' style={pillButton} onClick={addUtility}
+                                className='mx-2'>
+                            Add Utility
+                        </Button>
+                    </div>
+                )}
+            </Container>
 
-        </Container>
+        </div>
     );
 }
 
