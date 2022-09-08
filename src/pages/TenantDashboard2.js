428d8a801f1fdd68117a3cf6bfed09e9ee4f789b
Viha123 <sviha195@gmail.com>
2022-09-08 14:23:06 -0700

    works with new endpoint!

--- src/pages/TenantDashboard2.js
+++ src/pages/TenantDashboard2.js
@@ -12,84 +12,19 @@ import AppContext from "../AppContext";
 //tenant get request: https://t00axvabvb.execute-api.us-west-1.amazonaws.com/dev/tenantProfileInfo
 export default function TenantDashboard2(){
 
-    const [tenantData, setTenantData] = React.useState([]) 
     const [propertyData, setPropertyData] = React.useState([])
     const navigate = useNavigate();
     const { userData, refresh } = useContext(AppContext);
     const { access_token, user } = userData;
-    // const getProfileFromApi = () => { //process to get data from aateButtons(pi using axios
-    //     axios.get('https://t00axvabvb.execute-api.us-west-1.amazonaws.com/dev/tenantProfileInfo?',{
-    //         headers: {
-    //             // 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MjYwODg4MywianRpIjoiZTk4YThmNmEtNjUxMC00Y2IyLWJkM2EtNzQ3ZmUxNDIxNDJlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VyX3VpZCI6IjEwMC0wMDAwMTgiLCJmaXJzdF9uYW1lIjoiVmloYSIsImxhc3RfbmFtZSI6IlNoYWgiLCJwaG9uZV9udW1iZXIiOiI0MDg3Njc4MTU3IiwiZW1haWwiOiJ2aWhhLnNoYWhAc2pzdS5lZHUiLCJyb2xlIjoiVEVOQU5ULE9XTkVSIiwiZ29vZ2xlX2F1dGhfdG9rZW4iOiJ5YTI5LmEwQVZBOXkxdGxDd2RzZEwtTVg1SnFaN3l1Xy1MY2phZXFLZm5aNlE0SGZvY0cwaHMzQ3d2RVhmaFowUUxQcGs2RzN5STN6U3o1bUNaMGJqMHhvaEcxNDBDZHgzY1BBZzlsQ08xQ2NVUXRqZ3pER1NvM3dqdE1pRDMwRUllVjF5LWVISU85TUdEcnlzaWVfVDZpaXBHSTE2ODRoOS1JYUNnWUtBVEFTQVFBU0ZRRTY1ZHI4enA2NHVpS3kzMFI0UVptOW52TDBMQTAxNjMiLCJidXNpbmVzc2VzIjpbXX0sIm5iZiI6MTY2MjYwODg4MywiZXhwIjoxNjYyNjEyNDgzfQ.PehfaVCiNWlYNDy9fjn7MJc9Rqb9ajlCr2S2YpzHIa4'
-    //             // 'Authorization': 'Bearer ' + access_token
-    //             'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MjY1Nzg5MiwianRpIjoiNjg1ZWU2ZTktYWEzMi00YjFlLWI5NWYtMjI2MjZmZGVkOWEwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VyX3VpZCI6IjEwMC0wMDAwMTgiLCJmaXJzdF9uYW1lIjoiVmloYSIsImxhc3RfbmFtZSI6IlNoYWgiLCJwaG9uZV9udW1iZXIiOiI0MDg3Njc4MTU3IiwiZW1haWwiOiJ2aWhhLnNoYWhAc2pzdS5lZHUiLCJyb2xlIjoiVEVOQU5ULE9XTkVSIiwiZ29vZ2xlX2F1dGhfdG9rZW4iOiJ5YTI5LmEwQVZBOXkxdGxDd2RzZEwtTVg1SnFaN3l1Xy1MY2phZXFLZm5aNlE0SGZvY0cwaHMzQ3d2RVhmaFowUUxQcGs2RzN5STN6U3o1bUNaMGJqMHhvaEcxNDBDZHgzY1BBZzlsQ08xQ2NVUXRqZ3pER1NvM3dqdE1pRDMwRUllVjF5LWVISU85TUdEcnlzaWVfVDZpaXBHSTE2ODRoOS1JYUNnWUtBVEFTQVFBU0ZRRTY1ZHI4enA2NHVpS3kzMFI0UVptOW52TDBMQTAxNjMiLCJidXNpbmVzc2VzIjpbXX0sIm5iZiI6MTY2MjY1Nzg5MiwiZXhwIjoxNjYyNjYxNDkyfQ.3hVON7s7-lTYDXsERvRko_xKKJ5GLffmHH85Wscp9iY'
-    //         }
-    //     })
-    //     .then(response => {
-    //       setTenantData(response.data) //useState is getting the data
-          
     
-    //     }).catch(err =>{
-    //       console.log(err)
-    //     })
-    
-    // }
-    // const getPropertiesFromApi = () => { //process to get data from aateButtons(pi using axios
-    //     axios.get('https://t00axvabvb.execute-api.us-west-1.amazonaws.com/dev/tenantProperties?',{
-    //         headers: {
-    //             'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MjY1Nzg5MiwianRpIjoiNjg1ZWU2ZTktYWEzMi00YjFlLWI5NWYtMjI2MjZmZGVkOWEwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VyX3VpZCI6IjEwMC0wMDAwMTgiLCJmaXJzdF9uYW1lIjoiVmloYSIsImxhc3RfbmFtZSI6IlNoYWgiLCJwaG9uZV9udW1iZXIiOiI0MDg3Njc4MTU3IiwiZW1haWwiOiJ2aWhhLnNoYWhAc2pzdS5lZHUiLCJyb2xlIjoiVEVOQU5ULE9XTkVSIiwiZ29vZ2xlX2F1dGhfdG9rZW4iOiJ5YTI5LmEwQVZBOXkxdGxDd2RzZEwtTVg1SnFaN3l1Xy1MY2phZXFLZm5aNlE0SGZvY0cwaHMzQ3d2RVhmaFowUUxQcGs2RzN5STN6U3o1bUNaMGJqMHhvaEcxNDBDZHgzY1BBZzlsQ08xQ2NVUXRqZ3pER1NvM3dqdE1pRDMwRUllVjF5LWVISU85TUdEcnlzaWVfVDZpaXBHSTE2ODRoOS1JYUNnWUtBVEFTQVFBU0ZRRTY1ZHI4enA2NHVpS3kzMFI0UVptOW52TDBMQTAxNjMiLCJidXNpbmVzc2VzIjpbXX0sIm5iZiI6MTY2MjY1Nzg5MiwiZXhwIjoxNjYyNjYxNDkyfQ.3hVON7s7-lTYDXsERvRko_xKKJ5GLffmHH85Wscp9iY' 
-    //         }
-    //     })
-    //     .then(response => {
-    //         setPropertyData(response.data) //useState is getting the data
-          
-    
-    //     }).catch(err =>{
-    //       console.log(err)
-    //     })
-    
-    // }
-    // React.useEffect(()=>{ //gets the data from api only once. 
-    //     getProfileFromApi();
-    //     getPropertiesFromApi();
-    // },[]) //empty brackets prevents stuff from refreshing
-    // // console.log(tenantData.result)
-    // console.log(tenantData)
-    // console.log(propertyData)
-    //THIS ENTIRE SECTION MAKES IT SO THAT SOMEONE NAMED VIHA WHO IS A TENANT CAN ONLY ACCESS THE TENANTS INFORMATION
-    
-    const [profile, setProfile] = useState([]);
-    const [properties, setProperties] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const fetchProfile = async () => {
-        // const response = await get("/tenantProperties", access_token);
+    
+      const fetchTenantDashboard = async () => {
         if (access_token === null || user.role.indexOf("TENANT") === -1) {
           navigate("/");
           return;
         }
-        const response = await get("/tenantProfileInfo", access_token);
-        console.log("first");
-        console.log(response);
-    
-        if (response.msg === "Token has expired") {
-          console.log("here msg");
-          refresh();
-    
-          return;
-        }
-        //Extracts user profile
-        setTenantData(response);
-      };
-    
-      /*
-       * Fetches tenant properties based on user access_token
-       */
-      const fetchUserProperties = async () => {
-        if (access_token === null || user.role.indexOf("TENANT") === -1) {
-          navigate("/");
-          return;
-        }
-        const response = await get("/tenantProperties", access_token);
+        const response = await get("/tenantDashboard", access_token);
         console.log("second");
         console.log(response);
         setIsLoading(false);
@@ -103,62 +38,31 @@ export default function TenantDashboard2(){
         setPropertyData(response);
       };
     useEffect(() => {
-    // if (access_token === null) {
-    //   navigate("/");
-    // }
-    console.log("in use effect");
-    fetchProfile();
-    fetchUserProperties();
+        console.log("in use effect");
+        fetchTenantDashboard();
     }, []);
-
-    useEffect(() => {
-        // if (access_token === null) {
-        //   navigate("/");
-        // }
-        console.log("in second use effect");
-        if (access_token === null || user.role.indexOf("TENANT") === -1) {
-            navigate("/");
-            return;
-          }
-          const response = axios.get("https://t00axvabvb.execute-api.us-west-1.amazonaws.com/dev/tenantProfileInfo?", 
-            {headers: {
-                        'Authorization': access_token
-                    }}
-          );
-          console.log("first");
-          console.log(response);
-      
-          if (response.msg === "Token has expired") {
-            console.log("here msg");
-            refresh();
-      
-            return;
-          }
-          //Extracts user profile
-        //   setProfile(response.result[0]);
-        }, []);
-    console.log(profile)
+    console.log(propertyData)
     console.log(access_token)
     //END OF POSSIBLY IMPORTANT STUFF
     return(
         <div>
             
-            {tenantData.length !== 0 && <TopBar 
-                firstName={tenantData.result[0].tenant_first_name}
-                lastName={tenantData.result[0].tenant_last_name}
+            {propertyData.length !== 0 && <TopBar 
+                firstName={propertyData.result[0].tenant_first_name}
+                lastName={propertyData.result[0].tenant_last_name}
             />}
              <SideBar/>
             {propertyData.length !== 0 && <TenantCard 
-                imgSrc = {propertyData.result[0].images}
-                leaseEnds = {propertyData.result[0].active_date}
-                address1 = {propertyData.result[0].address}
-                city = {propertyData.result[0].city}
-                state = {propertyData.result[0].state}
-                zip = {propertyData.result[0].zip}
-                cost = {propertyData.result[0].listed_rent}
-                beds = {propertyData.result[0].num_beds}
-                bath = {propertyData.result[0].num_baths}
-                size = {propertyData.result[0].area}
+                imgSrc = {propertyData.result[0].properties[0].images}
+                leaseEnds = {propertyData.result[0].properties[0].active_date}
+                address1 = {propertyData.result[0].properties[0].address}
+                city = {propertyData.result[0].properties[0].city}
+                state = {propertyData.result[0].properties[0].state}
+                zip = {propertyData.result[0].properties[0].zip}
+                cost = {propertyData.result[0].properties[0].listed_rent}
+                beds = {propertyData.result[0].properties[0].num_beds}
+                bath = {propertyData.result[0].properties[0].num_baths}
+                size = {propertyData.result[0].properties[0].area}
 
             />}
             {/* <div className="announcements">
@@ -166,8 +70,8 @@ export default function TenantDashboard2(){
                 <h3>Tenant must evacuate within 3 months</h3>
             </div> */}
              {propertyData.length !== 0 && <UpcomingPayments
-                 data = {propertyData.result[0].tenantExpenses}
-             />}
+                 data = {propertyData.result[0].properties[0].tenantExpenses}
+             /> }
 
     </div>
     )
