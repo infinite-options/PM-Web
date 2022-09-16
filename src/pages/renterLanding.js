3e93407aee995fa0e4a0e1b07ebf5253ac1d4fc7
Viha123 <sviha195@gmail.com>
2022-09-16 09:08:15 -0700

    renterlading updates

--- src/pages/renterLanding.js
+++ src/pages/renterLanding.js
@@ -3,6 +3,8 @@ import LandingNavbar from "./LandingNavbar"
 import RenterMap from "../icons/RenterRoadMap.webp"
 import Options from "../icons/RenterRoadMap.webp"
 import "./renterLand.css";
+import PropertyCard from "../components/tenantComponents/PropertyCard"
+// import {PropImg} from "../icons/propImg.webp"
 export default function renderLanding(){
     return(
         <div className="main-renter-page">
@@ -17,6 +19,45 @@ export default function renderLanding(){
             </div>
             <h1>Schedule A Tour Today</h1>
             <h1>Featured Properties</h1>
+            {/* <PropertyCard propImg={PropImg}/> */}
+            <div className="p-container">
+                <div>
+                    <PropertyCard 
+                        color="#628191" 
+                        add1="1234 Market St, San Francisco"
+                        cost="$2700"
+                        bedrooms = "1"
+                        bathrooms = "1"
+
+                    />
+                    <PropertyCard 
+                        color="#FB8500" 
+                        add1="1234 Market St, San Francisco"
+                        cost="$2700"
+                        bedrooms = "1"
+                        bathrooms = "1"
+
+                    />
+                </div>
+                <div>
+                    <PropertyCard 
+                        color="rgb(255,183,3)" 
+                        add1="1234 Market St, San Francisco"
+                        cost="$2700"
+                        bedrooms = "1"
+                        bathrooms = "1"
+
+                    />
+                    <PropertyCard 
+                        color="rgb(33,158,188)" 
+                        add1="1234 Market St, San Francisco"
+                        cost="$2700"
+                        bedrooms = "1"
+                        bathrooms = "1"
+
+                    />
+                </div>
+            </div>
             
         </div>
     )
