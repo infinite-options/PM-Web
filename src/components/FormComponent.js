dbcc59510a0453c2ae4e69f7137111c5206c33d2
prashant2469 <89878340+prashant2469@users.noreply.github.com>
2022-09-18 14:37:25 -0700

    fixed margin on owner page, work in progress for the sign up button and input

---
+++ src/components/FormComponent.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import "../components/ownerComponentt/ownerstab.css";
+const emailState = {
+    email: '',
+    error: ''
+}
+class FormComponent extends React.Component {  
+    
+    constructor(){
+        super();
+        this.state = emailState;
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(e) {
+        this.setState({
+            email : e.target.value
+        });
+    }
+    emailValidation(){
+        const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!this.state.email || regex.test(this.state.email) === false){
+            this.setState({
+                error: "Email is not valid"
+            });
+            return false;
+        }
+        return true;
+    }
+    onSubmit(){
+        if(this.emailValidation()){
+            console.log(this.state);
+            this.setState(emailState);
+        }
+    }
+    render(){
+        return(
+            <div className='signupinput'>
+                {/* <label>Enter your email here *</label> */}
+                <div className="d-flex justify-content-center">
+                    
+                    <input type="email" name="email" value={this.state.email} onChange={this.onChange} className="inputval" />
+                    <span className="text-danger">{this.state.error}</span>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <button type="submit" className="btns" onClick={()=>this.onSubmit()}><span className='buttondescription'>Sign Up</span></button>
+                </div>
+                  
+            </div>
+        )  
+    }
+}  
+   
+export default FormComponent;
