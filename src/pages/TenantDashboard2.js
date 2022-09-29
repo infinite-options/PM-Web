898ff3b54935c7f102aa8853af85c6510b56ba49
Viha123 <sviha195@gmail.com>
2022-09-29 05:44:57 -0700

    links and dashboard for unregistered tenant

--- src/pages/TenantDashboard2.js
+++ src/pages/TenantDashboard2.js
@@ -21,7 +21,7 @@ export default function TenantDashboard2() {
   const { access_token, user } = userData;
   const [maintenanceRequests, setMaintenanceRequests] = React.useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [p, setP] = React.useState([]);
   const fetchTenantDashboard = async () => {
     if (access_token === null || user.role.indexOf("TENANT") === -1) {
       navigate("/");
@@ -29,6 +29,8 @@ export default function TenantDashboard2() {
     }
     const response = await get("/tenantDashboard", access_token);
     console.log(response);
+    setPropertyData(response);
+    setP(response.result[0]);
     setIsLoading(false);
 
     if (response.msg === "Token has expired") {
@@ -37,7 +39,7 @@ export default function TenantDashboard2() {
 
       return;
     }
-    setPropertyData(response);
+    
   };
   const getMaintenanceRequests = () => { //process to get data from aateButtons(pi using axios
     axios.get('https://t00axvabvb.execute-api.us-west-1.amazonaws.com/dev/maintenanceRequests')
@@ -53,7 +55,6 @@ export default function TenantDashboard2() {
     fetchTenantDashboard();
     getMaintenanceRequests();
   }, []);
-  console.log(maintenanceRequests);
   const goToMaintenence = () => {
     navigate("/maintenencePage", {
       state: {
@@ -62,89 +63,99 @@ export default function TenantDashboard2() {
       },
     });
   };
-  // console.log(propertyData)
   // console.log(propertyData.result[0]);
   //END OF POSSIBLY IMPORTANT STUFF
-  return (
-    <div>
-      {propertyData.length !== 0 && (
-        // <TopBar
-        //   firstName={propertyData.result[0].tenant_first_name}
-        //   lastName={propertyData.result[0].tenant_last_name}
-        // />
-        // <div className="topBar">
-        //   <div className="circle"></div>
-        //   <div>
-        //     <h1>{propertyData.result[0].tenant_first_name}</h1>
-        //     <h2>Tenant</h2>
-        //   </div>
-        // </div>
-        <div>
-          <h3 style={{paddingLeft: "7rem", paddingTop:"2rem"}}>{propertyData.result[0].tenant_first_name}</h3> 
-          <h8 style={{paddingLeft: "7rem", paddingTop:"2rem"}}>Tenant</h8>
-          </div>
-      )}
-    
+  // console.log(p);
+  // return null
+  //PROBLEMS:
+  //1.the if else statement does not seem to work because of undefined readings
+  // console.log(p)
+  console.log(isLoading);
+  console.log(propertyData)
+  console.log(p.properties)
 
-      <div className="flex-1">
-        <div className="sidebar">
-          <SideBar />
-        </div>
-        <div className="main-content">
-          <br />
-          <div className="box1">
-            {propertyData.length !== 0 && (
-              <TenantCard
-                imgSrc={propertyData.result[0].properties[0].images}
-                leaseEnds={propertyData.result[0].properties[0].active_date}
-                address1={propertyData.result[0].properties[0].address}
-                city={propertyData.result[0].properties[0].city}
-                state={propertyData.result[0].properties[0].state}
-                zip={propertyData.result[0].properties[0].zip}
-                cost={propertyData.result[0].properties[0].listed_rent}
-                beds={propertyData.result[0].properties[0].num_beds}
-                bath={propertyData.result[0].properties[0].num_baths}
-                size={propertyData.result[0].properties[0].area}
-              />
-            )}
-            <button className="b yellow" onClick ={goToMaintenence}>Submit Maintenence Ticket</button>
-            <button className="b">Contact Property Manager</button>
-          </div>
-          <div className="box2">
-            <div className="announcements">
-              Announcements
-              <h3 className="ann"></h3>
-            </div>
-            
-            {propertyData.length !== 0 && (
-              <UpcomingPayments
-                data={propertyData.result[0].properties[0].tenantExpenses}
-                type = {true}
-              />
-            )}
-            {propertyData.length !== 0 && <PaymentHistory data={propertyData.result[0].properties[0].tenantExpenses} />}
-          </div>
-        </div>
-      </div>
-      <div className="flex-2">
-        {propertyData.length !== 0 && <Maintenence 
-                                        data={maintenanceRequests.result}
-                                        address = {propertyData.result[0].properties[0].address} />}
-        {propertyData.length !== 0 && (
+  return (
+    // p.length > 0?
+    <div>
+    {propertyData !== undefined && isLoading === false && (<div>
+      {console.log("on line 80")}
+      {propertyData!== undefined && p.properties?.length > 0 ? 
+ 
+        <div>
+      
+        {propertyData?.length !== 0 && (
           <div>
-            <Appliances
-              data={propertyData.result[0].properties[0].appliances}
-            />
-            <PersonalInfo id = "profile" data={propertyData.result[0]} />
+            <h3 style={{paddingLeft: "7rem", paddingTop:"2rem"}}>{propertyData.result[0].tenant_first_name}</h3> 
+            <h8 style={{paddingLeft: "7rem", paddingTop:"2rem"}}>Tenant</h8>
           </div>
         )}
-      </div>
+        <div className="flex-1">
+          <div className="sidebar">
+            <SideBar uid = {propertyData.result[0].properties[0].property_uid}/>
+          </div>
+          <div className="main-content">
+            <br />
+            <div className="box1">
+              {propertyData.length !== 0 && (
+                <TenantCard
+                  imgSrc={propertyData.result[0].properties[0]?.images}
+                  leaseEnds={propertyData.result[0].properties[0]?.active_date}
+                  address1={propertyData.result[0].properties[0]?.address}
+                  city={propertyData.result[0].properties[0]?.city}
+                  state={propertyData.result[0].properties[0]?.state}
+                  zip={propertyData.result[0].properties[0]?.zip}
+                  cost={propertyData.result[0].properties[0]?.listed_rent}
+                  beds={propertyData.result[0].properties[0]?.num_beds}
+                  bath={propertyData.result[0].properties[0]?.num_baths}
+                  size={propertyData.result[0].properties[0]?.area}
+                  property={propertyData.result[0].properties[0]?.property_uid}
+                />
+              )}
+              <button className="b yellow" onClick ={goToMaintenence}>Submit Maintenence Ticket</button>
+              <button className="b">Contact Property Manager</button>
+            </div>
+            <div className="box2">
+              <div className="announcements">
+                Announcements
+                <h3 className="ann"></h3>
+              </div>
+              
+              {propertyData.length !== 0 && (
+                <UpcomingPayments
+                  data={propertyData.result[0].properties[0]?.tenantExpenses}
+                  type = {true}
+                />
+              )}
+              {propertyData.length !== 0 && <PaymentHistory data={propertyData.result[0].properties[0]?.tenantExpenses} />}
+            </div>
+          </div>
+        </div>
+        <div className="flex-2">
+          {propertyData.length !== 0 && <Maintenence 
+                                          data={maintenanceRequests.result}
+                                          address = {propertyData.result[0].properties[0]?.address} />}
+          {propertyData.length !== 0 && (
+            <div>
+              <Appliances
+                data={propertyData.result[0].properties[0]?.appliances}
+              />
+              <PersonalInfo id = "profile" data={propertyData.result[0]} />
+            </div>
+          )}
+        </div>
 
-      {/* <div className="profile">
-              {propertyData.length !== 0 && <PersonalInfo 
-              data = {propertyData.result[0].tenantInfo[0]}
-              />}
-            </div> */}
-    </div>
+      </div>
+      :
+        <div>
+          {/* {console.log(propertyData)}
+          {console.log("not working correctly")} */}
+            {navigate('/tenant_original')}
+
+        </div>
+      }
+
+
+    </div>)}
+      </div>
   );
 }
