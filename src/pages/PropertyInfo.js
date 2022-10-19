27b6d96b77736bb1db4bbf5aec65546e43197247
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-10-19 09:08:38 -0700

    resolving conflict

--- src/pages/PropertyInfo.js
+++ src/pages/PropertyInfo.js
@@ -12,11 +12,9 @@ import ReviewPropertyLease from "./reviewPropertyLease";
 export default function PropertyInfo() {
   const location = useLocation();
   const navigate = useNavigate();
-  console.log(location.state);
   const data = location.state.property;
   const type = location.state.type;
   const imgs = data.images;
-  console.log(imgs);
   const util = Object.entries(JSON.parse(data.utilities));
   // const [currentImg, setCurrentImg] = React.useState(0);
   const [currentImg, setCurrentImg] = React.useState(0);
@@ -49,33 +47,45 @@ export default function PropertyInfo() {
         </ol>
       );
     }
-
-    const nextImg = () => {
-        if (currentImg === imgs.length - 1) {
-          setCurrentImg(0);
-        } else {
-          setCurrentImg((prev) => prev+1);
-        }
-      };
-      const previousImg = () => {
-        if (currentImg === 0) {
-          setCurrentImg(imgs.length - 1);
-        } else {
-          setCurrentImg((prev)=> prev- 1);
-        }
-      };
-    const showImgs = imgs?.map((img)=>{
-        return(
-            <img className="more-info-prop-images" src={img}/>
-        )
-    })
-    console.log("location state: ");
-    console.log(location.state);
-    return(
-        <div className="prop-info-container">
-            {imgs.length>3?
-                <h3 className="prop-info-images-container" >
-                    {/* <div onClick={previousImg} className="left-arrow">{"<"}</div>
+  });
+  const tenantUtils = util?.map((u) => {
+    if (u[1] == false) {
+      counter3 += 1;
+      return (
+        <ol>
+          {counter3}. {u[0]}
+        </ol>
+      );
+    }
+  });
+  const goToApplyToProperty = () => {
+    // navigate("/applyToProperty");
+    navigate(`/tenantPropertyView/${data.property_uid}`);
+  };
+  const nextImg = () => {
+    if (currentImg === imgs.length - 1) {
+      setCurrentImg(0);
+    } else {
+      setCurrentImg((prev) => prev + 1);
+    }
+  };
+  const previousImg = () => {
+    if (currentImg === 0) {
+      setCurrentImg(imgs.length - 1);
+    } else {
+      setCurrentImg((prev) => prev - 1);
+    }
+  };
+  const showImgs = imgs?.map((img) => {
+    return <img className="more-info-prop-images" src={img} />;
+  });
+  console.log("location state: ");
+  console.log(location.state);
+  return (
+    <div className="prop-info-container">
+      {imgs.length > 3 ? (
+        <h3 className="prop-info-images-container">
+          {/* <div onClick={previousImg} className="left-arrow">{"<"}</div>
                     <img className="more-info-prop-images" src={imgs[currentImg]}/>
                     <div onClick={nextImg} className="right-arrow">{">"}</div>  */}
           <Carousel breakPoints={breakPoints}>{showImgs}</Carousel>
@@ -85,66 +95,11 @@ export default function PropertyInfo() {
           {/* <div onClick={previousImg} className="left-arrow">{"<"}</div>
                     <img className="more-info-prop-images" src={imgs[currentImg]}/>
                     <div onClick={nextImg} className="right-arrow">{">"}</div>  */}
-                    {showImgs}
-                </h3>
-                
-            }
-            
-            <h1 className="prop-info-address">{data.address}</h1>
-            
-            <h3 className="prop-info-prop-type">{data.property_type}</h3>
-            <div className="info-container">
-                <div className="left box">
-                    <h3 className="prop-info-stats">Rent : ${data.listed_rent} / mo</h3>
-                    <h3 className="prop-info-stats">Bedrooms: {data.num_beds}</h3>
-                    <h3 className="prop-info-stats">Bathrooms: {data.num_baths}</h3>
-                    <h3 className="prop-info-stats">Active Since: {data.active_date}</h3>
-                    <h3 className="prop-info-stats">Area : {data.area} SqFt</h3>
-                    <h3 className="prop-info-stats">City: {data.city}</h3>
-                    <div className= "contacts" style={{marginTop: "10vh"}}>
-                        <div>
-                            {/* {console.log("im trying to print an apply button")} */}
-                            <img src={Apply} onClick={goToApplyToProperty} alt="documentIcon"  />
-                            <div className="mask flex-center">
-                                <p className="white-text" style={{fontSize:"14px"}}>Apply</p>
-                            </div>
-                        </div>
-                        <div>
-                            <img
-                                onClick={() =>
-                                    (window.location.href = `tel:${location.state.property.business_number}`)
-                                    }
-                                src={Phone}
-                                style={{marginRight:"10px"}}
-                            />
-                            <div className="mask flex-center">
-                                <p className="white-text" style={{fontSize:"14px" ,marginRight:"0px"}}>Call</p>
-                            </div>
+          {showImgs}
+        </h3>
+      )}
 
-                        </div>
-                        <div>
-                        <img
-                            onClick={() =>
-                                (window.location.href = `mailto:${location.state.property. business_email}`)
-                                }
-                                src={Message}
-                                style={{marginRight:"10px"}}
-                            />
-                            <div className="mask flex-center">
-                                <p className="white-text" style={{fontSize:"14px",marginLeft:"0px"}}>Email</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                {/* <Appliances data = {data.appliances}/> */}
-                <div className="right box">
-                    <h3 className="prop-info-stats">Included Utilities <br/></h3>
-                    {availUtils}
-                    <h3 className="prop-info-stats">Utilities covered by Tenant <br/></h3>
-                    {tenantUtils}
-                    <h3 className="prop-info-stats">Included Appliances:</h3>
-                    {apps}
+      <h1 className="prop-info-address">{data.address}</h1>
 
       <h3 className="prop-info-prop-type">{data.property_type}</h3>
       <div className="info-container">
@@ -172,7 +127,7 @@ export default function PropertyInfo() {
             <div>
               <img
                 onClick={() =>
-                  (window.location.href = `tel:${location.state.business_number}`)
+                  (window.location.href = `tel:${location.state.property.business_number}`)
                 }
                 src={Phone}
                 style={{ marginRight: "10px" }}
@@ -189,7 +144,7 @@ export default function PropertyInfo() {
             <div>
               <img
                 onClick={() =>
-                  (window.location.href = `mailto:${location.state.business_email}`)
+                  (window.location.href = `mailto:${location.state.property.business_email}`)
                 }
                 src={Message}
                 style={{ marginRight: "10px" }}
