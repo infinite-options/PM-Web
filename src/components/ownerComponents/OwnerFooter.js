012886bc64d1e2248ad28a49a2fa3aad3f6e67e9
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-11-17 08:07:36 -0800

    made owner pages responsive

--- src/components/ownerComponents/OwnerFooter.js
+++ src/components/ownerComponents/OwnerFooter.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { smallBlue, smallGray } from "../../utils/styles";
+import { smallGray } from "../../utils/styles";
 import Dashboard_Blue from "../../icons/Dashboard_Blue.svg";
 import Dashboard_Gray from "../../icons/Dashboard_Gray.svg";
 import Contacts_Gray from "../../icons/Contacts_Gray.svg";
@@ -17,7 +17,6 @@ import AppContext from "../../AppContext";
 
 function OwnerFooter(props) {
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState("Dashboard");
   const footerContainer = {
     backgroundColor: "#F5F5F5",
     borderTop: "1px solid #EAEAEA",
@@ -33,108 +32,67 @@ function OwnerFooter(props) {
       <Col
         className="text-center"
         onClick={() => {
-          setIsActive("Dashboard");
           navigate("/owner");
         }}
       >
-        <img
-          src={isActive === "Dashboard" ? Dashboard_Blue : Dashboard_Gray}
-          alt="Dashboard"
-        />
-        <p
-          style={isActive === "Dashboard" ? smallBlue : smallGray}
-          className="mb-0"
-        >
+        <img src={Dashboard_Gray} alt="Dashboard" />
+        <p style={smallGray} className="mb-0">
           Dashboard
         </p>
       </Col>
       <Col
         className="text-center"
         onClick={() => {
-          setIsActive("Utilities");
-          navigate("/owner-utilities");
-        }}
-      >
-        <img
-          src={isActive === "Utilities" ? Expenses_Blue : Expenses_Gray}
-          alt="EXPENSES"
-        />
-        <p
-          style={isActive === "Utilities" ? smallBlue : smallGray}
-          className="mb-0"
-        >
-          Utilities
-        </p>
-      </Col>
-      <Col
-        className="text-center"
-        onClick={() => {
-          setIsActive("Contacts");
           navigate("/search-pm");
         }}
       >
-        <img
-          src={isActive === "Contacts" ? Contacts_Blue : Contacts_Gray}
-          alt="Contacts"
-        />
-        <p
-          style={isActive === "Contacts" ? smallBlue : smallGray}
-          className="mb-0"
-        >
+        <img src={Contacts_Gray} alt="Contacts" />
+        <p style={smallGray} className="mb-0">
           Contacts
         </p>
       </Col>
       <Col
         className="text-center"
         onClick={() => {
-          setIsActive("Maintenance");
           navigate("/owner-repairs");
         }}
       >
-        <img
-          src={isActive === "Maintenance" ? Repairs_Blue : Repairs_Gray}
-          alt="Repairs"
-        />
-        <p
-          style={isActive === "Maintenance" ? smallBlue : smallGray}
-          className="mb-0"
-        >
+        <img src={Repairs_Gray} alt="Repairs" />
+        <p style={smallGray} className="mb-0">
           Maintenance
+        </p>
+      </Col>
+      .
+      <Col
+        className="text-center"
+        onClick={() => {
+          navigate("/owner-utilities");
+        }}
+      >
+        <img src={Expenses_Gray} alt="EXPENSES" />
+        <p style={smallGray} className="mb-0">
+          Utilities
         </p>
       </Col>
       <Col
         className="text-center"
         onClick={() => {
-          setIsActive("Documents");
           navigate("/owner-documents");
         }}
       >
-        <img
-          src={isActive === "Documents" ? Profile_Blue : Profile_Gray}
-          alt="Profile"
-        />
-        <p
-          style={isActive === "Documents" ? smallBlue : smallGray}
-          className="mb-0"
-        >
+        <img src={Profile_Gray} alt="Profile" />
+        <p style={smallGray} className="mb-0">
           Documents
         </p>
       </Col>
       <Col
         className="text-center"
         onClick={() => {
-          setIsActive("Profile");
           navigate("/owner-profile");
         }}
       >
-        <img
-          src={isActive === "Profile" ? Profile_Blue : Profile_Gray}
-          alt="Profile"
-        />
-        <p
-          style={isActive === "Profile" ? smallBlue : smallGray}
-          className="mb-0"
-        >
+        <img src={Profile_Gray} alt="Profile" />
+        <p style={smallGray} className="mb-0">
           Profile
         </p>
       </Col>
