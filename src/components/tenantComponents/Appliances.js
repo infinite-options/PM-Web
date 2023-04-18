9cc8d1b0ce0461359cf8ad5f23568476bc35b269
aSandhu7893 <anu.sandhu7893@gmail.com>
2023-04-18 14:54:27 -0700

    appliances image and doc modal

--- src/components/tenantComponents/Appliances.js
+++ src/components/tenantComponents/Appliances.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import {
   Table,
   TableRow,
@@ -7,12 +7,17 @@ import {
   TableBody,
   TableHead,
 } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import { makeStyles } from "@material-ui/core/styles";
 import ImageModal from "../ImageModal";
 import File from "../../icons/File.svg";
 import ImgIcon from "../../icons/ImgIcon.png";
 import LinkIcon from "../../icons/LinkIcon.svg";
 import DocIcon from "../../icons/DocIcon.png";
+import { pillButton } from "../../utils/styles";
 const useStyles = makeStyles({
   customTable: {
     "& .MuiTableCell-sizeSmall": {
@@ -27,7 +32,15 @@ const useStyles = makeStyles({
 export default function Appliances(props) {
   const classes = useStyles();
   const { appliances, applianceState } = props;
-
+  const [applianceImgModalShow, setApplianceImgModalShow] = useState(false);
+  const [applianceDocModalShow, setApplianceDocModalShow] = useState(false);
+  const [applianceUrlModalShow, setApplianceUrlModalShow] = useState(false);
+  const [selectedAppliance, setSelectedAppliance] = useState(
+    Object.values(applianceState[0])[0]
+  );
+  const [openImage, setOpenImage] = useState(false);
+  const [imageSrc, setImageSrc] = useState(null);
+  console.log(Object.values(applianceState));
   const showImage = (src) => {
     setOpenImage(true);
     setImageSrc(src);
@@ -36,11 +49,147 @@ export default function Appliances(props) {
     setOpenImage(false);
     setImageSrc(null);
   };
-  const [openImage, setOpenImage] = useState(false);
-  const [imageSrc, setImageSrc] = useState(null);
-
+  const hideModal = () => {
+    setApplianceImgModalShow(false);
+    setApplianceDocModalShow(false);
+    setApplianceUrlModalShow(false);
+  };
+  const ApplianceImgModal = () => {
+    return (
+      <Dialog
+        open={applianceImgModalShow}
+        onClose={hideModal}
+        fullWidth
+        maxWidth="md"
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle closeButton>Images</DialogTitle>
+        <DialogContent>
+          {selectedAppliance["images"].length > 0 ? (
+            <div
+              className="row row-cols-3 flex-column"
+              style={{ height: "26rem" }}
+            >
+              {selectedAppliance["images"].map((img, i) => (
+                <div className="col d-flex justify-content-center py-2" key={i}>
+                  <div
+                    className="card"
+                    style={{ width: "14rem", height: "14rem" }}
+                  >
+                    <div className="card-body">
+                      <img
+                        src={img}
+                        style={{
+                          width: "12rem",
+                          height: "12rem",
+                          objectFit: "contain",
+                        }}
+                        onClick={() => showImage(img)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button style={pillButton} onClick={hideModal} color="primary">
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  const ApplianceDocModal = () => {
+    return (
+      <Dialog
+        open={applianceDocModalShow}
+        onClose={hideModal}
+        fullWidth
+        maxWidth="md"
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle closeButton>Documents</DialogTitle>
+        <DialogContent>
+          {" "}
+          {selectedAppliance["documents"].length > 0 ? (
+            <Table
+              responsive="md"
+              classes={{ root: classes.customTable }}
+              size="small"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>View</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {selectedAppliance["documents"].map((file, i) => {
+                  return (
+                    <TableRow>
+                      <TableCell>{file.name}</TableCell>
+                      <TableCell>{file.description}</TableCell>
+                      <TableCell>
+                        <a href={file.link} target="_blank" rel="noreferrer">
+                          <img
+                            src={File}
+                            alt="open document"
+                            style={{
+                              width: "15px",
+                              height: "15px",
+                            }}
+                          />
+                        </a>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          ) : (
+            ""
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button style={pillButton} onClick={hideModal} color="primary">
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  const ApplianceUrlModal = () => {
+    return (
+      <Dialog
+        open={applianceUrlModalShow}
+        onClose={hideModal}
+        fullWidth
+        maxWidth="md"
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle closeButton></DialogTitle>
+        <DialogContent></DialogContent>
+        <DialogActions>
+          <Button style={pillButton} onClick={hideModal} color="primary">
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
   return (
     <Row className="m-3" style={{ overflow: "scroll" }}>
+      {ApplianceImgModal()}
+      {ApplianceDocModal()}
+      {ApplianceUrlModal()}
       <ImageModal src={imageSrc} isOpen={openImage} onCancel={unShowImage} />
       <Table
         responsive="md"
@@ -103,6 +252,10 @@ export default function Appliances(props) {
                   applianceState[0][appliance]["url"].length > 0 ? (
                     <img
                       src={LinkIcon}
+                      onClick={() => {
+                        setApplianceUrlModalShow(!applianceUrlModalShow);
+                        setSelectedAppliance(applianceState[0][appliance]);
+                      }}
                       style={{
                         width: "15px",
                         height: "15px",
@@ -117,6 +270,10 @@ export default function Appliances(props) {
                   applianceState[0][appliance]["images"].length > 0 ? (
                     <img
                       src={ImgIcon}
+                      onClick={() => {
+                        setApplianceImgModalShow(!applianceImgModalShow);
+                        setSelectedAppliance(applianceState[0][appliance]);
+                      }}
                       style={{
                         width: "25px",
                         height: "25px",
@@ -131,6 +288,10 @@ export default function Appliances(props) {
                   applianceState[0][appliance]["documents"].length > 0 ? (
                     <img
                       src={DocIcon}
+                      onClick={() => {
+                        setApplianceDocModalShow(!applianceDocModalShow);
+                        setSelectedAppliance(applianceState[0][appliance]);
+                      }}
                       style={{
                         width: "20px",
                         height: "20px",
